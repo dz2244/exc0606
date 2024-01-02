bf955e51034e6247fa1b52de1d20f0ce1728506d
dz2244 <dz2244@bs.amalnet.k12.il>
2024-01-03 01:09:56 +0200

    commit 1

--- app/src/main/java/com/example/exc0606/MainActivity.java
+++ app/src/main/java/com/example/exc0606/MainActivity.java
@@ -7,15 +7,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    ImageView text ;
+    ImageView image ;
     Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        image = findViewById(R.id.image);
 
     }
     public void ace(View view) {
+        Random  rnd = new Random();
+        int x = rnd.nextInt(3)+1;
+        image.setImageResource(R.drawable.);
     }
 }
